85e2ddb8e33ec0d300086454104742467928806e
Ujjwal Upadhyay <ujjwal.2000upadhyay@gmail.com>
2025-04-14 19:29:50 +0530

    forntend developed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
   import { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import axios from 'axios';
-import Task from './Task';
-import Login from './login';
-import Register from './Register';
+import Task from './Task.jsx';
+import Login from './login.jsx';
+import Register from './Register.jsx';
 
 
 
